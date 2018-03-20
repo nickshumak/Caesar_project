888eed7d2429f2f5157d510e398ad2db6ad62c8c
kozinvl <vladiksystem@gmail.com>
2018-03-20 22:27:04 +0200

    Revert "Revert "updated""
    This reverts commit 412655343226b2f41b5a02d59894b671a9ad63f2.

--- сaesar/front/pages/groups_page.py
+++ сaesar/front/pages/groups_page.py
@@ -1,3 +1,16 @@
-class GroupsPage(object):
+from front.pages.base_page import BasePage
+
+
+class GroupsPage(BasePage):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
+
+    class LeftBar(object):
+        """inner classes"""
+        pass
+
+    class RightBar(object):
+        pass
+
+    class HeadBar(object):
+        pass
