d4d015c10a014f11264386549844320fbd88da8b
GeneRiabchenko <36885177+GeneRiabchenko@users.noreply.github.com>
2018-03-22 13:02:04 +0200

    Added test and auto_login function

--- сaesar/tests/test_base.py
+++ сaesar/tests/test_base.py
@@ -2,14 +2,15 @@ import unittest
 from selenium import webdriver
 from resource.url_site import PathUrl
 from resource.path_driver import GetDriver
+from front.pages.login_page import LogInPage
 
 
 class TestBase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(
-            executable_path=GetDriver.DRIVER_CHROME)
-        self.driver.get(PathUrl.URL_SITE)
-        self.driver.implicitly_wait(10)
+            executable_path=GetDriver().DRIVER_CHROME)
+        self.login_page = LogInPage(self.driver)
+        self.login_page.get(PathUrl().URL_SITE)
 
     def tearDown(self):
         self.driver.quit()
