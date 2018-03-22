64837550c3da04fce3076c644744e9aa2fea4e1f
GeneRiabchenko <36885177+GeneRiabchenko@users.noreply.github.com>
2018-03-22 13:07:20 +0200

    change button locator in login_page

--- сaesar/front/pages/login_page.py
+++ сaesar/front/pages/login_page.py
@@ -5,5 +5,5 @@ from page_objects import PageObject, PageElement
 class LogInPage(PageObject, BasePage):
     login = PageElement(name='login')
     password = PageElement(name='password')
-    submit = PageElement(css='body > div.app > div > div > div > button')
+    submit = PageElement(tag_name='button')
     massage = PageElement(class_name='message')
