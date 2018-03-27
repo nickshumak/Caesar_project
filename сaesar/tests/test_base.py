764a5ba914f743419aeffbb517b19f1157e05198
nshumakov <mastershumakov@gmail.com>
2018-03-28 00:38:30 +0300

    Added creating page logics(without adding expert)

--- сaesar/tests/test_base.py
+++ сaesar/tests/test_base.py
@@ -9,7 +9,7 @@ class TestBase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(
             executable_path=GetDriver().DRIVER_CHROME)
-        self.driver.implicitly_wait(5)
+        # self.driver.implicitly_wait(5)
         self.driver.get(PathUrl().URL_SITE)
         self.login_page = LogInPage(self.driver)
         self.driver.maximize_window()
--- сaesar/tests/test_creating_group.py
+++ сaesar/tests/test_creating_group.py
@@ -5,6 +5,8 @@ from tests.test_base import TestBase
 TEST_GROUP_NAME = "Test group"
 TEST_TOO_LONG_GROUP_NAME = "12345678901234567890"
 TEST_START_DATE = "11/04/2018"
+TEST_TEACHER_INDEX = 1
+TEST_FAILED_MESSAGE = "Test_failed"
 
 
 class TestCreatingGroup(TestBase):
@@ -15,14 +17,17 @@ class TestCreatingGroup(TestBase):
         left_menu = groups_page.left_menu_open()
         button_create_group = left_menu.create_group()
         button_create_group.click()
-        groups_page.WindowCreatingGroup().direction_of_group_select(2)
+        groups_page.WindowCreatingGroup().direction_of_group_random_select()
         groups_page.WindowCreatingGroup().location_of_group_random_select()
         test_direction = groups_page.WindowCreatingGroup().direction_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().date_start_setting(TEST_START_DATE)
-        groups_page.WindowCreatingGroup().teachers_adding(1)
+        groups_page.WindowCreatingGroup().teachers_adding(TEST_TEACHER_INDEX)
         name = groups_page.WindowCreatingGroup().name_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().submit_group_creating()
-        self.assertTrue(name in self.driver.page_source)
+        try:
+            self.assertTrue(name in str(self.driver.page_source))
+        except AssertionError:
+            print(TEST_FAILED_MESSAGE)
         # self.assertEqual(self.driver.current_url, expected_page_url)
 
     def test_is_group_creates_with_test_name(self):
@@ -31,17 +36,17 @@ class TestCreatingGroup(TestBase):
         left_menu = groups_page.left_menu_open()
         button_create_group = left_menu.create_group()
         button_create_group.click()
-        groups_page.WindowCreatingGroup().direction_of_group_select(2)
+        groups_page.WindowCreatingGroup().direction_of_group_random_select()
         groups_page.WindowCreatingGroup().location_of_group_random_select()
         groups_page.WindowCreatingGroup().group_name_setting(TEST_GROUP_NAME)
         groups_page.WindowCreatingGroup().date_start_setting(TEST_START_DATE)
-        groups_page.WindowCreatingGroup().teachers_adding(1)
+        groups_page.WindowCreatingGroup().teachers_adding(TEST_TEACHER_INDEX)
         name = groups_page.WindowCreatingGroup().name_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().submit_group_creating()
         try:
-            self.assertTrue(name in self.driver.page_source)
-        except:
-            self.assertFalse(name in self.driver.page_source)
+            self.assertIn(name, str(self.driver.page_source))
+        except AssertionError:
+            print(TEST_FAILED_MESSAGE)
 
     def test_is_group_creates_with_test_location(self):
         first_admin.auto_login_n_open_group_page(self.login_page)
@@ -49,17 +54,17 @@ class TestCreatingGroup(TestBase):
         left_menu = groups_page.left_menu_open()
         button_create_group = left_menu.create_group()
         button_create_group.click()
-        groups_page.WindowCreatingGroup().direction_of_group_select(2)
+        groups_page.WindowCreatingGroup().direction_of_group_random_select()
         groups_page.WindowCreatingGroup().location_of_group_random_select()
         location_of_group = groups_page.WindowCreatingGroup().location_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().date_start_setting(TEST_START_DATE)
-        groups_page.WindowCreatingGroup().teachers_adding(1)
+        groups_page.WindowCreatingGroup().teachers_adding(TEST_TEACHER_INDEX)
         name = groups_page.WindowCreatingGroup().name_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().submit_group_creating()
         try:
-            self.assertTrue(location_of_group in self.driver.page_source)
-        except:
-            self.assertFalse(location_of_group in self.driver.page_source)
+            self.assertIn(location_of_group, str(self.driver.page_source))
+        except AssertionError:
+             print(TEST_FAILED_MESSAGE)
 
     def test_is_group_creates_with_correct_start_date(self):
         first_admin.auto_login_n_open_group_page(self.login_page)
@@ -67,11 +72,33 @@ class TestCreatingGroup(TestBase):
         left_menu = groups_page.left_menu_open()
         button_create_group = left_menu.create_group()
         button_create_group.click()
-        groups_page.WindowCreatingGroup().direction_of_group_select(2)
+        groups_page.WindowCreatingGroup().direction_of_group_random_select()
         groups_page.WindowCreatingGroup().location_of_group_random_select()
         test_direction = groups_page.WindowCreatingGroup().direction_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().date_start_setting(TEST_START_DATE)
-        groups_page.WindowCreatingGroup().teachers_adding(1)
+        groups_page.WindowCreatingGroup().teachers_adding(TEST_TEACHER_INDEX)
         name = groups_page.WindowCreatingGroup().name_of_group_save_to_variable()
         groups_page.WindowCreatingGroup().submit_group_creating()
-        self.assertTrue(TEST_START_DATE in self.driver.page_source)
+        try:
+            self.assertIn(TEST_START_DATE, str(self.driver.page_source))
+        except AssertionError:
+             print(TEST_FAILED_MESSAGE)
+
+    def test_is_group_creates_direction_right(self):
+        first_admin.auto_login_n_open_group_page(self.login_page)
+        groups_page = GroupsPage(self.driver)
+        left_menu = groups_page.left_menu_open()
+        button_create_group = left_menu.create_group()
+        button_create_group.click()
+        groups_page.WindowCreatingGroup().direction_of_group_random_select()
+        groups_page.WindowCreatingGroup().location_of_group_random_select()
+        test_direction = groups_page.WindowCreatingGroup().direction_of_group_save_to_variable()
+        groups_page.WindowCreatingGroup().date_start_setting(TEST_START_DATE)
+        groups_page.WindowCreatingGroup().teachers_adding(TEST_TEACHER_INDEX)
+        name = groups_page.WindowCreatingGroup().name_of_group_save_to_variable()
+        groups_page.WindowCreatingGroup().submit_group_creating()
+        try:
+            self.assertIn(test_direction, str(self.driver.page_source))
+        except AssertionError:
+            print(TEST_FAILED_MESSAGE)
+        # self.assertEqual(self.driver.current_url, expected_page_url)
