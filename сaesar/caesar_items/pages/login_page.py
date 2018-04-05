acaedc1237a9f47da73fcdd3a1be7474852c0695
GeneRiabchenko <36885177+GeneRiabchenko@users.noreply.github.com>
2018-04-05 17:27:47 +0300

    Changed Login Page architecture. Added error messages if test fails in login page test suite.

--- сaesar/caesar_items/pages/login_page.py
+++ сaesar/caesar_items/pages/login_page.py
@@ -11,31 +11,25 @@ from caesar_items.pages.groups_page import GroupsPage
 class LogInPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
+        self.login_field = self.driver.find_element(*LogInLocators.LOGIN_FIELD)
+        self.password_field = self.driver.\
+            find_element(*LogInLocators.PASSWORD_FIELD)
+        self.submit_button = self.driver.find_element(*LogInLocators.CONFIRM_ACTION)
+        self.error_message = self.driver.find_element(*LogInLocators.FIELD_MESSAGE)
 
     def set_login_field_text(self, user_login=''):
         """ Enter user login in login field."""
-        login_field = self.driver.find_element(*LogInLocators.LOGIN_FIELD)
-        login_field.clear()
-        login_field.send_keys(user_login)
+        self.login_field.clear()
+        self.login_field.send_keys(user_login)
 
     def set_password_field_text(self, user_password=''):
         """ Enter user password in password field."""
-        password_field = self.driver.\
-            find_element(*LogInLocators.PASSWORD_FIELD)
-        password_field.clear()
-        password_field.send_keys(user_password)
-
-    def get_message_text(self):
-        """ Get error message above login field."""
-        return self.driver.find_element(*LogInLocators.FIELD_MESSAGE).text
-
-    def submit_button(self):
-        """ Get submit button web element."""
-        return self.driver.find_element(*LogInLocators.CONFIRM_ACTION)
+        self.password_field.clear()
+        self.password_field.send_keys(user_password)
 
     def click_submit_button(self):
         """ Click on submit button and wait for user photo."""
-        self.driver.find_element(*LogInLocators.CONFIRM_ACTION).click()
+        self.submit_button.click()
         try:
             WebDriverWait(self.driver, 5).until(
                 EC.visibility_of_element_located(GroupPageLocators.USER_PHOTO))
--- сaesar/tests/test_suite_login_page.py
+++ сaesar/tests/test_suite_login_page.py
@@ -9,86 +9,103 @@ from caesar_items.pages.login_page import LogInPage
 
 expected_message = 'Incorrect login or password. Please, try again'
 expected_page_title = 'Caesar'
+group_page_not_opened = "Group page didn't opened."
+user_name_not_matched = "User's name didn't matched."
+user_role_not_matched = "User's role didn't matched."
 
 
 class TestLoginPage(TestBase):
-    @classmethod
-    def setUpClass(cls):
-        """ Create 8 users for tests."""
-        cls.driver = webdriver.Chrome(
-            executable_path=GetDriver().CHROME_DRIVER)
-        cls.driver.get(PathUrl().SITE_URL)
-        cls.login_page = LogInPage(cls.driver)
-        cls.login_page.set_login_field_text(first_admin.login)
-        cls.login_page.set_password_field_text(first_admin.password)
-        cls.group_page = cls.login_page.click_submit_button()
-        cls.group_page.open_admin_page()
-        create_8_users_for_tests(cls.driver)
-        cls.driver.quit()
+    # @classmethod
+    # def setUpClass(cls):
+    #     """ Create 8 users for tests."""
+    #     cls.driver = webdriver.Chrome(
+    #         executable_path=GetDriver().CHROME_DRIVER)
+    #     cls.driver.get(PathUrl().SITE_URL)
+    #     cls.login_page = LogInPage(cls.driver)
+    #     cls.login_page.set_login_field_text(first_admin.login)
+    #     cls.login_page.set_password_field_text(first_admin.password)
+    #     cls.group_page = cls.login_page.click_submit_button()
+    #     cls.group_page.open_admin_page()
+    #     create_8_users_for_tests(cls.driver)
+    #     cls.driver.quit()
 
     def test01_submit_button_enabled(self):
         """ Check that button is active when login and password entered."""
         self.login_page.set_login_field_text(first_admin.login)
         self.login_page.set_password_field_text(first_admin.password)
-        self.assertTrue(self.login_page.submit_button().is_enabled())
+        self.assertTrue(self.login_page.submit_button.is_enabled(),
+                        "Submit button disabled.")
 
     def test02_login_as_admin(self):
         """ Sign in as admin and check his name and role on page."""
         self.login_page.set_login_field_text(first_admin.login)
         self.login_page.set_password_field_text(first_admin.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
         right_menu = self.group_page.open_right_menu()
         self.assertEqual(right_menu.get_user_full_name_text(),
-                         first_admin.full_name)
-        self.assertEqual(right_menu.get_user_role_text(), first_admin.role)
+                         first_admin.full_name, user_name_not_matched )
+        self.assertEqual(right_menu.get_user_role_text(), first_admin.role,
+                         user_role_not_matched)
 
     def test03_login_as_coordinator(self):
         """ Sign in as coordinator and check his name and role on page."""
         self.login_page.set_login_field_text(coordinator.login)
         self.login_page.set_password_field_text(coordinator.password)
         self.group_page = self.login_page.click_submit_button()
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
         right_menu = self.group_page.open_right_menu()
-        self.assertEqual(right_menu.get_user_full_name_text(), coordinator.full_name)
-        self.assertEqual(right_menu.get_user_role_text(), coordinator.role)
+        self.assertEqual(right_menu.get_user_full_name_text(), coordinator.full_name,
+                         user_name_not_matched)
+        self.assertEqual(right_menu.get_user_role_text(), coordinator.role,
+                         user_role_not_matched)
 
     def test04_sign_in_as_teacher(self):
         """ Sign in as teacher and check his name and role on page."""
         self.login_page.set_login_field_text(teacher.login)
         self.login_page.set_password_field_text(teacher.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
         right_menu = self.group_page.open_right_menu()
-        self.assertEqual(right_menu.get_user_full_name_text(), teacher.full_name)
-        self.assertEqual(right_menu.get_user_role_text(), teacher.role)
+        self.assertEqual(right_menu.get_user_full_name_text(), teacher.full_name,
+                         user_name_not_matched)
+        self.assertEqual(right_menu.get_user_role_text(), teacher.role,
+                         user_role_not_matched)
 
     def test05_length_password_equal_4(self):
         """ Check that user with password length equal 4 can sign in on site."""
         self.login_page.set_login_field_text(user_password_len_4.login)
         self.login_page.set_password_field_text(user_password_len_4.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     def test06_length_password_equal_10(self):
         """ Check that user with password length equal 10 can sign in on site."""
         self.login_page.set_login_field_text(user_password_len_10.login)
         self.login_page.set_password_field_text(user_password_len_10.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     def test07_length_login_equal_4(self):
         """ Check that user with login length equal 4 can sign in on site."""
         self.login_page.set_login_field_text(user_login_len_4.login)
         self.login_page.set_password_field_text(user_login_len_4.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     def test08_length_login_equal_10(self):
         """ Check that user with password length equal 10 can sign in on site."""
         self.login_page.set_login_field_text(user_login_len_10.login)
         self.login_page.set_password_field_text(user_login_len_10.password)
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     def test09_sensitive_to_case(self):
         """ Check that upper case of user login and password
@@ -96,7 +113,8 @@ class TestLoginPage(TestBase):
         self.login_page.set_login_field_text(first_admin.login.upper())
         self.login_page.set_password_field_text(first_admin.password.upper())
         self.group_page = self.login_page.click_submit_button()
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     def test10_check_enter_n_tab_buttons(self):
         """ Check tab and enter keys:
@@ -104,26 +122,27 @@ class TestLoginPage(TestBase):
         enter - confirm action(login on site)"""
         self.group_page = self.login_page.\
             login_use_tab_n_enter_keys(first_admin)
-        self.assertEqual(self.group_page.get_title_name(), expected_page_title)
+        self.assertEqual(self.group_page.get_title_name(), expected_page_title,
+                         group_page_not_opened)
 
     # negative tests
     def test11_password_field_empty_submit_button_is_disabled(self):
         """ Check if password field is empty, submit button disabled."""
         self.login_page.set_login_field_text(first_admin.login)
         self.login_page.set_password_field_text()
-        self.assertFalse(self.login_page.submit_button().is_enabled())
+        self.assertFalse(self.login_page.submit_button.is_enabled())
 
     def test12_login_field_empty_submit_button_is_disabled(self):
         """ Check if login field is empty, submit button disabled."""
         self.login_page.set_login_field_text()
         self.login_page.set_password_field_text(first_admin.password)
-        self.assertFalse(self.login_page.submit_button().is_enabled())
+        self.assertFalse(self.login_page.submit_button.is_enabled())
 
     def test13_all_fields_empty_submit_button_is_disabled(self):
         """ Check if all field is empty, submit button disabled."""
         self.login_page.set_login_field_text()
         self.login_page.set_password_field_text()
-        self.assertFalse(self.login_page.submit_button().is_enabled())
+        self.assertFalse(self.login_page.submit_button.is_enabled())
 
     def test14_length_login_equal_3(self):
         """ Check that user with login length equal 3 can not sign in on site.
@@ -131,7 +150,7 @@ class TestLoginPage(TestBase):
         self.login_page.set_login_field_text(user_login_len_3.login)
         self.login_page.set_password_field_text(user_login_len_3.password)
         self.login_page.click_submit_button()
-        self.assertEqual(self.login_page.get_message_text(), expected_message)
+        self.assertEqual(self.login_page.error_message.text, expected_message)
 
     def test15_length_login_equal_11(self):
         """ Check that user with login length equal 11 can not sign in on site.
@@ -139,7 +158,7 @@ class TestLoginPage(TestBase):
         self.login_page.set_login_field_text(user_login_len_11.login)
         self.login_page.set_password_field_text(user_login_len_11.password)
         self.login_page.click_submit_button()
-        self.assertEqual(self.login_page.get_message_text(), expected_message)
+        self.assertEqual(self.login_page.error_message.text, expected_message)
 
     def test16_length_password_equal_3(self):
         """ Check that user with password length equal 3 can not sign in on site.
@@ -147,7 +166,7 @@ class TestLoginPage(TestBase):
         self.login_page.set_login_field_text(user_password_len_3.login)
         self.login_page.set_password_field_text(user_password_len_3.password)
         self.login_page.click_submit_button()
-        self.assertEqual(self.login_page.get_message_text(), expected_message)
+        self.assertEqual(self.login_page.error_message.text, expected_message)
 
     def test17_length_password_equal_11(self):
         """ Check that user with password length equal 11 can not sign in on site.
@@ -155,7 +174,7 @@ class TestLoginPage(TestBase):
         self.login_page.set_login_field_text(user_password_len_11.login)
         self.login_page.set_password_field_text(user_password_len_11.password)
         self.login_page.click_submit_button()
-        self.assertEqual(self.login_page.get_message_text(), expected_message)
+        self.assertEqual(self.login_page.error_message.text, expected_message)
 
 
 if __name__ == '__main__':
